707dd5d28190c1506ea9e365e951854e9c8dc073
Vinaya Acharya <vinay.acharya05@gmail.com>
2025-12-31 19:09:21 +0530

    Route handlers

--- src/app.js
+++ src/app.js
@@ -6,13 +6,31 @@ const app = express();
 //     res.send("Hello from / !!!");
 // });
 
-app.use('/test',(req, res) => {
-    res.send("Hello from /test !!!");
+app.get(/.*fly/,(req, res) => {
+    res.send("Executed");
 });
 
-app.use('/hello',(req, res) => {
-    res.send("Hello from /hello !!!");
+// app.get('/user/:id/:name',(req, res) => {
+//     console.log("Path param", req.params);
+//     res.send({name: "Vinay", age: 24});
+// });
+
+// app.get('/user',(req, res) => {
+//     console.log("Dynamic query param", req.query);
+//     res.send({name: "Vinay111", age: 24});
+// });
+
+app.post('/user', (req, res) => {
+    console.log("POst request received");
+    res.send({message: "Data saved successfully"});
 });
+
+
+app.delete('/user',(req, res) => {
+    res.send("User deleted successfully");
+});
+
+
 
 // app.use((req, res) => {
 //     res.send("Hello from the server!!!");
